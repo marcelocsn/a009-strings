7bbf921a8a392101778e988d08edc39b98167599
csnicolela.marcelo@gmail.com <csnicolela.marcelo@gmail.com>
2022-08-31 16:19:12 -0300

    Exercícios aula 09 dia 31/08

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,9 @@
 //Crie a const para a frase aqui
+const frase = "Renato tem um xbox azul e preto, esse xbox possui um recado em postit com os dizeres: \"VIDEOGAME quebrado não ligue\""
+
+const novaFrase = frase.replace("azul", "preto").replace("verde","laranja")
+
+console.log(`Frase com as substituições: ${novaFrase}`)
+
+const fraseMaius = "quebrado não ligue".toUpperCase()
+console.log(`Frase maiuscula: ${novaFrase.replace(`quebrado não ligue`, fraseMaius)}`)
